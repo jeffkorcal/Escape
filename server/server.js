482869f9b0568b3343f96aa63294b192d6d9692a
Jeff Korcal <jkorcal@gmail.com>
2016-07-11 16:46:31 -0700

    serve static assets through server

--- server/server.js
+++ server/server.js
@@ -1,22 +1,66 @@
 ////////////////////////////////////////////////
 //Modules
 ////////////////////////////////////////////////
+
+var images = require('../env/images.json');
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-Parser');
+var request = require('request');
+var fs = require('fs');
 
+// declare server
 var app = express();
 
-mongoose.connect('mongodb://localhost/escape');
+////////////////////////////////////////////////
+//Middleware
+////////////////////////////////////////////////
 
-var db = mongoose.connection;
+// Parse JSON (uniform resource locators)
+app.use(bodyParser.json());
 
-db.on('error', console.error);
-db.once('open', function() {
-  // Create your schemas and models here.
+// Parse Forms (signup/login)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve Static Assets
+app.use(express.static(__dirname + '/../client'));
+
+
+app.listen(8000, function() {
+  console.log('listening on port 8000');
 });
 
-
-
-app.listen(8000);
-
 module.exports = app;
+
+
+// mongoose.connect('mongodb://localhost/escape');
+// var db = mongoose.connection;
+// db.on('error', console.error);
+// db.once('open', function() {
+//   // Create your schemas and models here.
+// });
+
+// var imageSchema = new mongoose.Schema({
+//   format: String,
+//   width: Number,
+//   height: Number,
+//   filename: String,
+//   id: Number,
+//   author: String,
+//   author_url:String,
+//   post_url: String
+// });
+
+// var Image = mongoose.model('Image', imageSchema);
+
+// snippet to download json list from unsplash.it
+// request('https://unsplash.it/list', function (err, res, body) {
+//   if (!err && res.statusCode == 200) {
+//     fs.writeFile('test.json', body, 'utf8', function (err) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       return ('transfered')
+//     });
+//   }
+// });
