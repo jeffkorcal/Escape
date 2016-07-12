8102e1618385b974dcb0788b08983d70843087d1
Jeff Korcal <jkorcal@gmail.com>
2016-07-11 23:09:31 -0700

    connect frontend events with backend

--- client/app/app.js
+++ client/app/app.js
@@ -1,11 +1,55 @@
 var app = angular.module('escape', []);
 
+
+app.controller('menuController', function($scope, Image) {
+  $scope.author = '';
+  $scope.credit = '';
+  $scope.url = '';
+
+  $scope.changePicture = function() {
+    Image.getRandomPic().then(function(data){
+      $scope.author = data.author;
+      $scope.credit = data.author_url;
+      $scope.url = 'https://unsplash.it/800/600?image=' + data.id;
+    });
+  };
+
+  $scope.grayScale = function() {
+    var url = $scope.url;
+    var urlArr = url.split('');
+    urlArr.splice(20, 0, 'g', '/');
+    url = urlArr;
+    $scope.url = url.join('');
+  };
+
+  $scope.changePicture();
+
+});
+
 app.controller('timeController', function($scope, $interval) {
     
-    var tick = function() {
-      $scope.today = Date.now();
-    }
-    tick();
-    $interval(tick, 1000);
+  var tick = function() {
+    $scope.today = Date.now();
+  }
+  tick();
+  $interval(tick, 1000);
 
-  });
+});
+
+app.factory('Image', function ($http) {
+
+  var getRandomPic = function () {
+    return $http({
+      method: 'GET',
+      url: '/api/images/'
+    }).then(function (res) {
+      return res.data;
+    });
+  }
+
+  return {
+    getRandomPic: getRandomPic
+  } 
+
+});
+
